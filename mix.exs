bdb31a5216d4ea7a26760d521a3b38e04565e134
crcrios <crcrios@bancolombia.com.co>
2022-12-20 13:42:28 -0500

    test sonar

--- mix.exs
+++ mix.exs
@@ -27,6 +27,7 @@ defmodule OracleService.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:jason, "~> 1.3"},
       {:distillery, "~> 2.1"}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
